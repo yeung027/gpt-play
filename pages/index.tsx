3ee1cdc8cbc5bd0ffda4f4fa90fa5a548ed766d0
HeyHei <shinghei.yeung@gmail.com>
2023-03-22 14:17:15 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,149 +33,149 @@ const Home: NextPage = () => {
   return (
 
       
-<div class="container mx-auto shadow-lg rounded-lg">
-        <!-- headaer -->
-    <div class="px-5 py-5 flex justify-between items-center bg-white border-b-2">
-      <div class="font-semibold text-2xl">GoingChat</div>
-      <div class="w-1/2">
+<div className="container mx-auto shadow-lg rounded-lg">
+      
+    <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
+      <div className="font-semibold text-2xl">GoingChat</div>
+      <div className="w-1/2">
         <input
           type="text"
           name=""
           id=""
           placeholder="search IRL"
-          class="rounded-2xl bg-gray-100 py-3 px-5 w-full"
+          className="rounded-2xl bg-gray-100 py-3 px-5 w-full"
         />
       </div>
       <div
-        class="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center"
+        className="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center"
       >
         RA
       </div>
     </div>
-    <!-- end header -->
-    <!-- Chatting -->
-    <div class="flex flex-row justify-between bg-white">
-      <!-- chat list -->
-      <div class="flex flex-col w-2/5 border-r-2 overflow-y-auto">
-        <!-- search compt -->
-        <div class="border-b-2 py-4 px-2">
+    
+    
+    <div className="flex flex-row justify-between bg-white">
+      
+      <div className="flex flex-col w-2/5 border-r-2 overflow-y-auto">
+        
+        <div className="border-b-2 py-4 px-2">
           <input
             type="text"
             placeholder="search chatting"
-            class="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
+            className="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
           />
         </div>
-        <!-- end search compt -->
-        <!-- user list -->
+        
+        
         <div
-          class="flex flex-row py-4 px-2 justify-center items-center border-b-2"
+          className="flex flex-row py-4 px-2 justify-center items-center border-b-2"
         >
-          <div class="w-1/4">
+          <div className="w-1/4">
             <img
               src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
-              class="object-cover h-12 w-12 rounded-full"
+              className="object-cover h-12 w-12 rounded-full"
               alt=""
             />
           </div>
-          <div class="w-full">
-            <div class="text-lg font-semibold">Luis1994</div>
-            <span class="text-gray-500">Pick me at 9:00 Am</span>
+          <div className="w-full">
+            <div className="text-lg font-semibold">Luis1994</div>
+            <span className="text-gray-500">Pick me at 9:00 Am</span>
           </div>
         </div>
-        <div class="flex flex-row py-4 px-2 items-center border-b-2">
-          <div class="w-1/4">
+        <div className="flex flex-row py-4 px-2 items-center border-b-2">
+          <div className="w-1/4">
             <img
               src="https://source.unsplash.com/otT2199XwI8/600x600"
-              class="object-cover h-12 w-12 rounded-full"
+              className="object-cover h-12 w-12 rounded-full"
               alt=""
             />
           </div>
-          <div class="w-full">
-            <div class="text-lg font-semibold">Everest Trip 2021</div>
-            <span class="text-gray-500">Hi Sam, Welcome</span>
+          <div className="w-full">
+            <div className="text-lg font-semibold">Everest Trip 2021</div>
+            <span className="text-gray-500">Hi Sam, Welcome</span>
           </div>
         </div>
         <div
-          class="flex flex-row py-4 px-2 items-center border-b-2 border-l-4 border-blue-400"
+          className="flex flex-row py-4 px-2 items-center border-b-2 border-l-4 border-blue-400"
         >
-          <div class="w-1/4">
+          <div className="w-1/4">
             <img
               src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
-              class="object-cover h-12 w-12 rounded-full"
+              className="object-cover h-12 w-12 rounded-full"
               alt=""
             />
           </div>
-          <div class="w-full">
-            <div class="text-lg font-semibold">MERN Stack</div>
-            <span class="text-gray-500">Lusi : Thanks Everyone</span>
+          <div className="w-full">
+            <div className="text-lg font-semibold">MERN Stack</div>
+            <span className="text-gray-500">Lusi : Thanks Everyone</span>
           </div>
         </div>
-        <div class="flex flex-row py-4 px-2 items-center border-b-2">
-          <div class="w-1/4">
+        <div className="flex flex-row py-4 px-2 items-center border-b-2">
+          <div className="w-1/4">
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-12 w-12 rounded-full"
+              className="object-cover h-12 w-12 rounded-full"
               alt=""
             />
           </div>
-          <div class="w-full">
-            <div class="text-lg font-semibold">Javascript Indonesia</div>
-            <span class="text-gray-500">Evan : some one can fix this</span>
+          <div className="w-full">
+            <div className="text-lg font-semibold">Javascript Indonesia</div>
+            <span className="text-gray-500">Evan : some one can fix this</span>
           </div>
         </div>
-        <div class="flex flex-row py-4 px-2 items-center border-b-2">
-          <div class="w-1/4">
+        <div className="flex flex-row py-4 px-2 items-center border-b-2">
+          <div className="w-1/4">
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-12 w-12 rounded-full"
+              className="object-cover h-12 w-12 rounded-full"
               alt=""
             />
           </div>
-          <div class="w-full">
-            <div class="text-lg font-semibold">Javascript Indonesia</div>
-            <span class="text-gray-500">Evan : some one can fix this</span>
+          <div className="w-full">
+            <div className="text-lg font-semibold">Javascript Indonesia</div>
+            <span className="text-gray-500">Evan : some one can fix this</span>
           </div>
         </div>
 
-        <div class="flex flex-row py-4 px-2 items-center border-b-2">
-          <div class="w-1/4">
+        <div className="flex flex-row py-4 px-2 items-center border-b-2">
+          <div className="w-1/4">
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-12 w-12 rounded-full"
+              className="object-cover h-12 w-12 rounded-full"
               alt=""
             />
           </div>
-          <div class="w-full">
-            <div class="text-lg font-semibold">Javascript Indonesia</div>
-            <span class="text-gray-500">Evan : some one can fix this</span>
+          <div className="w-full">
+            <div className="text-lg font-semibold">Javascript Indonesia</div>
+            <span className="text-gray-500">Evan : some one can fix this</span>
           </div>
         </div>
         <!-- end user list -->
       </div>
       <!-- end chat list -->
       <!-- message -->
-      <div class="w-full px-5 flex flex-col justify-between">
-        <div class="flex flex-col mt-5">
-          <div class="flex justify-end mb-4">
+      <div className="w-full px-5 flex flex-col justify-between">
+        <div className="flex flex-col mt-5">
+          <div className="flex justify-end mb-4">
             <div
-              class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+              className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
             >
               Welcome to group everyone !
             </div>
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-8 w-8 rounded-full"
+              className="object-cover h-8 w-8 rounded-full"
               alt=""
             />
           </div>
-          <div class="flex justify-start mb-4">
+          <div className="flex justify-start mb-4">
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-8 w-8 rounded-full"
+              className="object-cover h-8 w-8 rounded-full"
               alt=""
             />
             <div
-              class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+              className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
             >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
               at praesentium, aut ullam delectus odio error sit rem. Architecto
@@ -183,17 +183,17 @@ const Home: NextPage = () => {
               consequatur quas?
             </div>
           </div>
-          <div class="flex justify-end mb-4">
+          <div className="flex justify-end mb-4">
             <div>
               <div
-                class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
               >
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Magnam, repudiandae.
               </div>
 
               <div
-                class="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
               >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Debitis, reiciendis!
@@ -201,42 +201,42 @@ const Home: NextPage = () => {
             </div>
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-8 w-8 rounded-full"
+              className="object-cover h-8 w-8 rounded-full"
               alt=""
             />
           </div>
-          <div class="flex justify-start mb-4">
+          <div className="flex justify-start mb-4">
             <img
               src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-              class="object-cover h-8 w-8 rounded-full"
+              className="object-cover h-8 w-8 rounded-full"
               alt=""
             />
             <div
-              class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+              className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
             >
               happy holiday guys!
             </div>
           </div>
         </div>
-        <div class="py-5">
+        <div className="py-5">
           <input
-            class="w-full bg-gray-300 py-5 px-3 rounded-xl"
+            className="w-full bg-gray-300 py-5 px-3 rounded-xl"
             type="text"
             placeholder="type your message here..."
           />
         </div>
       </div>
       <!-- end message -->
-      <div class="w-2/5 border-l-2 px-5">
-        <div class="flex flex-col">
-          <div class="font-semibold text-xl py-4">Mern Stack Group</div>
+      <div className="w-2/5 border-l-2 px-5">
+        <div className="flex flex-col">
+          <div className="font-semibold text-xl py-4">Mern Stack Group</div>
           <img
             src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
-            class="object-cover rounded-xl h-64"
+            className="object-cover rounded-xl h-64"
             alt=""
           />
-          <div class="font-semibold py-4">Created 22 Sep 2021</div>
-          <div class="font-light">
+          <div className="font-semibold py-4">Created 22 Sep 2021</div>
+          <div className="font-light">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
             perspiciatis!
           </div>
